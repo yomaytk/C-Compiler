4d8ee77b52099999b627ace1f3b05f1914b76de0
yoma <ym.atonement.oy999@gmail.com>
2019-10-27 01:12:07 +0900

    make initial compiler

---
+++ test3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main(){
+	while(1){
+		char buff[1024];
+		scanf("%s",buff);
+		printf("%s HELLO \n",buff);
+	}
+}
---
+++ test3.s
@@ -0,0 +1,39 @@
+	.file	"test3.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"%s"
+.LC1:
+	.string	"%s HELLO \n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$1040, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+.L2:
+	leaq	-1040(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+	leaq	-1040(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC1(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	jmp	.L2
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 7.4.0-1ubuntu1~18.04.1) 7.4.0"
+	.section	.note.GNU-stack,"",@progbits
